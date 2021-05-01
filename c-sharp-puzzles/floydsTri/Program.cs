5634e266bb852db1cac05b91ab8a5efa8b875c5d
j4rryd <j.j.wimbridge@gmail.com>
2021-05-01 20:27:30 +0930

    chore: move csharp folder in [IMLAZY-000]

---
+++ c-sharp-puzzles/floydsTri/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FloydsTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void createFloyds(int height)
+        {
+            int curr = 1;
+            for (int lvl = 1; lvl <= height; lvl++)
+            {
+                string numLine = "";
+                while (numLine.Length <= lvl)
+                {
+                    numLine += String.
+                }
+            }
+        }
+    }
+}
---
+++ c-sharp-puzzles/reverseNums/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Puzzles
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+            reverseNumsArray(nums);
+        }
+
+        static void reverseNumsArray(int[] arr)
+        {
+            int total = arr.Length;
+            for (int i = 0; i <= total/2-1; i++)
+            {
+                int hold = arr[i];
+                arr[i] = arr[total-i-1];
+                arr[total-i-1] = hold;
+            }
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
